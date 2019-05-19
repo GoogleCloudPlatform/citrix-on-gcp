cf299614beeb2972a1b88b72eb2e4c7f57810286
Jeff Allen <jeffallen@google.com>
2019-05-19 03:20:33 -0400

    update vda version, stage but no execute chrome install

---
+++ bootstrap/install-chrome.ps1
@@ -0,0 +1,34 @@
+#
+#  Copyright 2019 Google Inc.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+
+# Install Chrome
+Write-Host "downloading chrome..."
+$url = "https://storage.googleapis.com/citrix-on-gcp-demo/chrome/googlechromestandaloneenterprise64.msi"
+$filetype = "msi"
+$TempFile = New-TemporaryFile
+$TempFile.MoveTo($TempFile.FullName + "." + $filetype)
+(New-Object System.Net.WebClient).DownloadFile($url, $TempFile.FullName)
+
+Write-Host "installing chrome..."
+$MSIArguments = @(
+    "/q"
+    "/i"
+    $TempFile.FullName
+)
+Start-Process "msiexec.exe" -ArgumentList $MSIArguments -Wait -NoNewWindow 
+
+Remove-Item $TempFile.FullName -Force
+
